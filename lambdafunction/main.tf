e7f62a5f78e2b7b2cc57bcbb282cc1002136f97d
lokeshbn <lokeshnarasimha353@gmail.com>
2023-08-27 11:56:47 +0530

    first commit repo

---
+++ lambdafunction/main.tf
@@ -0,0 +1,27 @@
+provider "aws"  {
+region = "ap-south-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraformbackedstatefilestore"  #mention bucket used to store the tfstatefile
+    key    = "tffiles.tfstate"                #filename for tfstate file 
+    region = "ap-south-1"
+  }
+}
+
+resource "aws_lambda_function" "my_function" {
+    role = "arn:aws:iam::681217613251:role/My_Lambda_Function"
+    function_name = "my_lambda_function"
+    runtime = "python3.8"
+    handler = "index.handler"
+    timeout = 300
+    filename = data.archive_file.lambda_function_archive.output_path
+    source_code_hash = filebase64sha256(data.archive_file.lambda_function_archive.output_path)
+}
+
+data "archive_file" "lambda_function_archive" {
+  type        = "zip"
+  source_dir  =  "./lambdafunction"  # Path to the directory containing your Lambda function code
+  output_path = "./lambdafunction.zip"   # Path where you want to save the generated zip archive
+}
